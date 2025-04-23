5ca58634e96bd369ba6d2065e1ab04126bf18313
Cris <criistiiniilla.1994@gmail.com>
2025-04-23 11:48:28 +0200

    proyecto-08 finalizado

--- proyecto-08/js/editarcliente.js
+++ proyecto-08/js/editarcliente.js
@@ -1,17 +1,59 @@
 (function(){
 
+    let DB;
+    let idCliente;
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+    const formulario = document.querySelector('#formulario');
+
     document.addEventListener('DOMContentLoaded', () => {
-        let DB;
         conectarDB();
 
+        formulario.addEventListener('submit', actualizarCliente);
+
         const parametrosURL = new URLSearchParams(window.location.search);
-        const idCliente = parametrosURL.get('id');
+        idCliente = parametrosURL.get('id');
         if(idCliente) {
             setTimeout(() => {
                 obtenerCliente(idCliente);
             }, 1000)
         }
     })
+
+    function actualizarCliente(e){
+        e.preventDefault();
+        if(nombreInput.value === '' || emailInput.value === '' || telefonoInput.value === '' || empresaInput.value === ''){
+            imprimirAlerta('Todos los campos son obligatorios.', 'error');
+            return;
+        }
+        const clienteActualizado = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: emailInput.value,
+            id: Number(idCliente),
+
+        }
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.put(clienteActualizado);
+
+        transaction.oncomplete = function(){
+            imprimirAlerta('Editado correctamente.');
+            
+            setTimeout(()=>{
+                window.location.href = 'index.html'
+            },3000)
+        }
+        
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error al editar el cliente', error)
+        }
+    }
 
     function obtenerCliente(id){
 
@@ -23,11 +65,20 @@
 
             if(cursor){
                 if(cursor.value.id === Number(id)){
-                    console.log(cursor.value)
+                    llenarFormulario(cursor.value)
                 }
                 cursor.continue();
             }
         }
+    }
+
+    function llenarFormulario(datosCliente){
+        const {nombre, email, telefono, empresa} = datosCliente;
+        nombreInput.value = nombre;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        empresaInput.value = empresa;
+
     }
 
     function conectarDB() {
